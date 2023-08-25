847adfb7e0d2152d787b1bdc964ff93c92702a51
Kompy <41587427+Kompreya@users.noreply.github.com>
2023-08-25 00:15:03 -0400

    Added command to dump stat IDs to file
    /statsdump will dump all stat IDs to statsdump.json at config/fmhelper

--- src/main/java/kombee/fmhelper/FMHelper.java
+++ src/main/java/kombee/fmhelper/FMHelper.java
@@ -1,22 +1,31 @@
 package kombee.fmhelper;
 
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.stats.StatisticsManagerServer;
+import net.minecraft.stats.StatBase;
+import net.minecraft.stats.StatList;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 import net.minecraft.world.storage.ISaveHandler;
 import net.minecraft.world.storage.SaveHandler;
-import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
-import net.minecraftforge.common.MinecraftForge;
 import com.google.gson.JsonObject;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import net.minecraft.entity.player.EntityPlayerMP;
 
 @Mod(modid = FMHelper.MODID, name = FMHelper.NAME, version = FMHelper.VERSION)
 public class FMHelper
@@ -25,6 +34,7 @@ public class FMHelper
     public static final String NAME = "Fancy Menu Helper";
     public static final String VERSION = "1.0";
     private static File configDir;
+
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         if (event.getSide().isClient()) {
@@ -52,13 +62,16 @@ public class FMHelper
             }
         }
     }
+
     @EventHandler
     public void init(FMLInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(this);
     }
+
     @EventHandler
     public void postInit(FMLPostInitializationEvent event) {
     }
+
     @SubscribeEvent
     public void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
 
@@ -87,6 +100,7 @@ public class FMHelper
             }
         }
     }
+
     private String getExactWorldFolderName(World world) {
         String worldFolderName = null;
 
@@ -98,7 +112,56 @@ public class FMHelper
 
         return worldFolderName;
     }
-    private boolean isSinglePlayer() {
-        return !FMLCommonHandler.instance().getMinecraftServerInstance().isDedicatedServer();
+
+    @EventHandler
+    public static void onServerStarting(FMLServerStartingEvent event) {
+        event.registerServerCommand(new CommandStatsDump());
+    }
+
+    public static class CommandStatsDump extends CommandBase {
+        @Override
+        public String getName() {
+            return "statsdump";
+        }
+
+        @Override
+        public String getUsage(ICommandSender sender) {
+            return "/statsdump";
+        }
+
+        @Override
+        public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+            if (args.length != 0) {
+                throw new CommandException("Invalid command. Use '/statsdump'.");
+            }
+
+            if (!(sender instanceof EntityPlayerMP)) {
+                sender.sendMessage(new TextComponentString("This command can only be used by players."));
+                return;
+            }
+
+            EntityPlayerMP player = (EntityPlayerMP) sender;
+            File statsDumpFile = new File(configDir, "statsdump.json");
+
+            try {
+                JsonObject statsData = new JsonObject();
+
+                StatisticsManagerServer statsManager = player.getStatFile();
+
+                for (StatBase stat : StatList.ALL_STATS) {
+                    int value = statsManager.readStat(stat);
+                    statsData.addProperty(stat.statId, value);
+                }
+
+                FileWriter writer = new FileWriter(statsDumpFile);
+                writer.write(statsData.toString());
+                writer.close();
+
+                sender.sendMessage(new TextComponentString("Statistics dumped to 'statsdump.json'"));
+            } catch (IOException e) {
+                FMLLog.severe("Failed to dump statistics: %s", e.getMessage());
+                sender.sendMessage(new TextComponentString("Failed to dump statistics. Check log for details."));
+            }
+        }
     }
 }
